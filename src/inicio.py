9ef2c974f6c151560b5923b5153f93fcd9925e32
pau <pzarante@uninorte.edu.co>
2024-03-18 00:54:42 -0500

    eduardo tennos paciencia porfi :D

--- src/inicio.py
+++ src/inicio.py
@@ -22,9 +22,9 @@ def main() -> None:
     sample_tree = generate_sample_abb()
     sample_tree.graficar()  # Genera el gráfico del árbol
     
-    # Menú de opciones
+   
     while True:
-        
+        # Menú principal de opciones
         print("OPERACIONES")
         print("1. Insertar nodo")
         print("2. Eliminar nodo")
@@ -37,25 +37,25 @@ def main() -> None:
         if option == "1":
             #Operación para insertar un nodo
             node_data = input("Ingrese el dato del nodo a insertar: ")
-            sample_tree.insert(node_data)
+            sample_tree.insert(node_data) # Inserta el nodo en el árbol
             sample_tree.graficar()  # Vuelve a generar el gráfico
 
         elif option == "2":
             #Operación para eliminar un nodo
             node_data = input("Ingrese el dato del nodo a eliminar: ")
-            sample_tree.delete(node_data)
+            sample_tree.delete(node_data) # Elimina el nodo del árbol
             sample_tree.graficar()  # Vuelve a generar el gráfico
 
         elif option == "3":
             #Operación para buscar un nodo
-            node_data = input("Ingrese el dato del nodo a buscar: ")
-            found_node = sample_tree.search(node_data)
+            node_data = input("Ingrese el dato del nodo a buscar: ") 
+            found_node = sample_tree.search(node_data) # Busca el nodo en el árbol
         
-            if found_node:
+            if found_node: # Si el nodo fue encontrado
                 print("El nodo se encuentra en el árbol.")
                 print("Desea realizar mas acciones con este nodo? (S/N)")
                 moreop= input("\n->Ingrese una opción: ")
-                
+                # Submenú de opciones para trabajar con el nodo encontrado
                 while(moreop=="s" or moreop=="S"):
                     print("a. Obtener nivel del nodo")
                     print("b. Obtener factor de Balanceo del nodo")
@@ -65,21 +65,26 @@ def main() -> None:
                     opp = input("\n->Ingrese una opción: ")
                     match(opp):
                         case "a":
+                            # Obtener nivel del nodo
                             print("El nivel del nodo es: ", sample_tree.get_nivel(found_node[0]))
                         case "b":
+                            # Obtener factor de balanceo del nodo
                             found_node[0].actualizar_factor_balance()
                             print("El factor de balanceo del nodo es: ", found_node[0].factor_balance)
                         case "c":
+                            # Obtener el padre del nodo - si existe
                             if(sample_tree.search_daddy(found_node[0].data)==None):
                                 print("El nodo no tiene padre.")
                             else:
                                 print("El padre del nodo es: ", sample_tree.search_daddy(found_node[0].data).data)
                         case "d":
+                            # Obtener el abuelo del nodo - si existe
                             if(sample_tree.search_granpa(found_node[0].data)==None):
                                 print("El nodo no tiene abuelo.")
                             else:
                                 print("El abuelo del nodo es: ", sample_tree.search_granpa(found_node[0].data).data)
                         case "e":
+                            # Obtener el tío del nodo - si existe
                             if(sample_tree.search_tio(found_node[0].data)==None):
                                 print("El nodo no tiene tío.")
                             else:
@@ -94,13 +99,13 @@ def main() -> None:
             # Buscar nodos por categoría y peso
             categoria = input("Ingrese la categoría: ").lower()
             print("Ingrese un rango (en bytes) del peso del archivo")
-            rango1 = float(input("De: "))
-            rango2 = float(input("Hasta: "))
-            nodos_encontrados= sample_tree.search_nodos_categoria_rango(categoria, rango1, rango2)
+            rango1 = float(input("De: ")) # Rango inferior
+            rango2 = float(input("Hasta: ")) # Rango superior
+            nodos_encontrados= sample_tree.search_nodos_categoria_rango(categoria, rango1, rango2) # Busca los nodos según rango de bytes y categoría
             if nodos_encontrados:
                 print("Nodos encontrados:")
                 for nodo in nodos_encontrados:
-                    print(nodo.data)
+                    print(nodo.data) # Imprime los nodos encontrados
                 
                 print("Desea realizar mas acciones con algun nodo? (S/N)")
                 moreop= input("\n->Ingrese una opción: ")
@@ -108,18 +113,18 @@ def main() -> None:
                 while(moreop=="s" or moreop=="S"):
                     
                     print ("Ingrese el nombre del nodo con el que desea trabajar")
-                    name = input("->Ingrese nombre: ")
-                    found = None
-                    while found == None:
-                        for nodo in nodos_encontrados:
+                    name = input("->Ingrese nombre: ") # Ingresa el nombre del nodo con el que se desea trabajar
+                    found = None 
+                    while found == None: # Verifica que el nodo ingresado sea válido
+                        for nodo in nodos_encontrados: # Busca el nodo en la lista de nodos encontrados
                             if nodo.data == name:
-                                found = nodo
+                                found = nodo # Si el nodo es válido, lo almacena
                                 break
                         if found == None:
                             print("El nodo no se encuentra en la lista de nodos encontrados.")
                             print ("\n Ingrese correctamente el nombre del nodo con el que desea trabajar")
                             name = input("->Ingrese nombre: ")
-                        
+                    # Submenú de opciones para trabajar con el nodo encontrado    
                     print("\na. Obtener nivel del nodo")
                     print("b. Obtener factor de Balanceo nodo")
                     print("c. Obtener el padre del nodo")
@@ -128,21 +133,26 @@ def main() -> None:
                     opp = input("\n->Ingrese una opción: ")
                     match(opp):
                         case "a":
+                            # Obtener nivel del nodo
                             print("El nivel del nodo es: ", sample_tree.get_nivel(found))
                         case "b":
+                            # Obtener factor de balanceo del nodo
                             found.actualizar_factor_balance()
                             print("El factor de balanceo del nodo es: ", found.factor_balance)
                         case "c":
+                            # Obtener el padre del nodo - si existe
                             if(sample_tree.search_daddy(found.data)==None):
                                 print("El nodo no tiene padre.")
                             else:
                                 print("El padre del nodo es: ", sample_tree.search_daddy(found.data).data)
                         case "d":
+                            # Obtener el abuelo del nodo - si existe
                             if(sample_tree.search_granpa(found.data)==None):
                                 print("El nodo no tiene abuelo.")
                             else:
                                 print("El abuelo del nodo es: ", sample_tree.search_granpa(found.data).data)
                         case "e":
+                            # Obtener el tío del nodo - si existe
                             if(sample_tree.search_tio(found.data)==None):
                                 print("El nodo no tiene tío.")
                             else:
